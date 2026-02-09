fade48402fa7af29a586cb5dca6bae5c3dac5b30
WillAnderson314 <will.anderson3141@gmail.com>
2026-02-09 11:24:26 -0700

    Restructured traveler to more cleanly work with algorithms and created file for algorithms

---
+++ algorithms.py
@@ -0,0 +1,14 @@
+# File for the different pathfinding algorithms, each defined as a seperate
+# function. Can split into multiple  iles for different algorithms later if 
+# that ends up being easier to work on
+
+# This could also be a class, but I'm not sure what doing that would add. 
+
+# Notes:
+# queue.sort(key=,ascending)
+# define key with function
+
+def dijkstra(start,goal,grid):
+    # Find shortest (or cheapest) path from
+    # start to goal on the grid usin dijkstra's algorithm
+    pass
--- traveler.py
+++ traveler.py
@@ -4,24 +4,29 @@
 
 class Traveler:
     def __init__(self,position,cellsize,algorithm):
+        '''traveler'''
 
         #Initialize grid and render positions
         self.position = position
         self.render_position = position * cellsize
         self.path = []
-        pass
 	
-    def follow_path(self,direction):
-	    # move 1 step along the path
-        pass
+    def follow_path(self):        
+        # Only act if there is a path to follow
+        if self.path != []:
+
+            # Move to next position in path, then remove that position from the path
+            self.pos = self.path[0]
+            self.path.pop(0)
 	
-    def find_path(self,algorithm,goal):
+    def find_path(self,algorithm,goal,grid):
         # Implement one of the algorithms to find the best path
-        path = []
+        # THIS IS DRAFT SYNTAX AND LIKELY WON'T WORK YET
+        path = algorithm(self.pos,goal,grid)
         self.path = path
 
     def draw(self):
-        # Many options to choose from here
+        # Many options to choose from here TODO
         pass
 
 
